ab9da6490042c791510fd67e905608ecefefae71
zac1st1k <zacfirst@gmail.com>
2015-02-09 15:27:54 +1100

    + Setup Search Controller

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,13 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchControllerDelegate, UISearchBarDelegate, UISearchResultsUpdating {
     
     var searchController: UISearchController!
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        searchController = UISearchController(searchResultsController: nil)
+
+        searchController.dimsBackgroundDuringPresentation = true
+        searchController.hidesNavigationBarDuringPresentation = true
+        searchController.searchBar.frame = CGRectMake(searchController.searchBar.frame.origin.x
+            , searchController.searchBar.frame.origin.y, searchController.searchBar.frame.size.height, 44.0)
+        tableView.tableHeaderView = searchController.searchBar
+        
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+//        definesPresentationContext = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,12 +34,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Dispose of any resources that can be recreated.
     }
     
-    //MARK: - UITableView Data Source
+    //MARK: - UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         return UITableViewCell()
+    }
+    //MARK: - UISearchResultsUpdating
+
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        
     }
 
 }
